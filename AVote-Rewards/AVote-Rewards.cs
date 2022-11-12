98eceee613b65d7a938968d35057ad7a71fdad90
Julian <julianmuchacho666@gmail.com>
2022-11-11 21:38:21 -0500

    init

---
+++ AVote-Rewards/AVote-Rewards.cs
@@ -0,0 +1,240 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
+using TShockAPI.Hooks;
+
+namespace AVote
+{
+    /// <summary>
+    /// The main plugin class should always be decorated with an ApiVersion attribute. The current API Version is 1.25
+    /// </summary>
+    [ApiVersion(2, 1)]
+    public class AVote : TerrariaPlugin
+    {
+        /// <summary>
+        /// The name of the plugin.
+        /// </summary>
+        public override string Name => "AVote-Rewards";
+
+        /// <summary>
+        /// The version of the plugin in its current state.
+        /// </summary>
+        public override Version Version => new Version(1, 0, 0);
+
+        /// <summary>
+        /// The author(s) of the plugin.
+        /// </summary>
+        public override string Author => "Average";
+
+        /// <summary>
+        /// A short, one-line, description of the plugin's purpose.
+        /// </summary>
+        public override string Description => "A simple, and light-weight Vote Rewards plugin.";
+        public static string apiKey;
+        public Config config;
+
+        /// <summary>
+        /// The plugin's constructor
+        /// Set your plugin's order (optional) and any other constructor logic here
+        /// </summary>
+        public AVote(Main game) : base(game)
+        {
+
+        }
+
+        /// <summary>
+        /// Performs plugin initialization logic.
+        /// Add your hooks, config file read/writes, etc here
+        /// </summary>
+        public override void Initialize()
+        {
+            if(apiKey == "xxx")
+            {
+                Console.WriteLine("VOTE REWARDS: You should probably set your api key in AVote.json in your tShock folder! If not, whatevs, you do you b.");
+            }
+
+            GeneralHooks.ReloadEvent += Reload;
+            ServerApi.Hooks.GameInitialize.Register(this, GameInit);
+
+        }
+
+        public void GameInit(EventArgs args)
+        {
+            Commands.ChatCommands.Add(new Command("av.vote", Vote, "vote"));
+            config = Config.Read();
+            apiKey = config.apiKey;
+        }
+        
+        public void Reload(ReloadEventArgs args)
+        {
+            args.Player.SendMessage("VoteRewards reloaded!", Color.LightGreen);
+            config = Config.Read();
+            apiKey = config.apiKey;
+        }
+
+        public void Vote(CommandArgs args)
+        {
+            if (!args.Player.IsLoggedIn)
+            {
+                args.Player.SendErrorMessage("You must be logged in first!");
+                return;
+            }
+
+            var isBeingUsedForTesting = false;
+
+            if(args.Parameters.Count == 1)
+            {
+                if(args.Parameters[0] == "-t")
+                {
+                    if (args.Player.HasPermission("av.admin"))
+                    {
+                        isBeingUsedForTesting = true;
+                        args.Player.SendInfoMessage("-t");
+                    }
+                }
+            }
+
+            TSPlayer Player = args.Player;
+
+            if (checkifPlayerVoted(Player).Result == true)
+            {
+                if(rewardClaimed(Player).Result == true || isBeingUsedForTesting == true)
+                {
+                    string rewardMsg = config.rewardMessage;
+                    rewardMsg = rewardMsg.Replace("%PLAYER%", Player.Name);
+                    TSPlayer.All.SendMessage(rewardMsg, Microsoft.Xna.Framework.Color.LightGreen);
+                    foreach (string cmd in config.Commands)
+                    {
+                        var newCmd = cmd.Replace("%PLAYER%", '"' + Player.Name + '"');
+                        Commands.HandleCommand(TSPlayer.Server, newCmd);
+                    }
+                    return;
+                }
+                else
+                {
+                    Player.SendErrorMessage("You have already claimed your reward for today!");
+                    return;
+                }
+
+            }
+
+            Player.SendMessage("You haven't voted today! Head to terraria-servers.com and vote for our server page!", Color.LightGreen);
+            return;
+        }
+
+        public static async Task<bool> rewardClaimed(TSPlayer player)
+        {
+            bool hasVoted = false;
+
+            string voteUrl = "http://terraria-servers.com/api/?action=post&object=votes&element=claim&key=" + apiKey + "&username=" + player.Name;
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    using (HttpResponseMessage res = await client.GetAsync(voteUrl))
+                    {
+                        using (HttpContent content = res.Content)
+                        {
+                            var data = await content.ReadAsStringAsync();
+
+                            if (data != null)
+                            {
+                                if (data == "1")
+                                {
+                                    hasVoted = true;
+                                    return hasVoted;
+                                }
+                                else
+                                {
+                                    hasVoted = false;
+                                    return hasVoted;
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("No data");
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception!!!!");
+                Console.Write(ex);
+                return hasVoted;
+            }
+
+            return hasVoted;
+
+        }
+
+        public static async Task<bool> checkifPlayerVoted(TSPlayer player)
+        {
+            bool hasVoted = false;
+
+            string voteUrl = "http://terraria-servers.com/api/?object=votes&element=claim&key=" + apiKey + "&username=" + player.Name;
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    using (HttpResponseMessage res = await client.GetAsync(voteUrl))
+                    {
+                        using (HttpContent content = res.Content)
+                        {
+                            var data = await content.ReadAsStringAsync();
+
+                            if (data != null)
+                            {
+                                if (data == "1" || data == "2")
+                                {
+                                    hasVoted = true;
+                                    return hasVoted;
+                                }
+                                else
+                                {
+                                    hasVoted = false;
+                                    return hasVoted;
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("No data");
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception!!!!");
+                Console.Write(ex);
+                return hasVoted;
+            }
+
+            return hasVoted;
+
+        }
+
+        /// <summary>
+        /// Performs plugin cleanup logic
+        /// Remove your hooks and perform general cleanup here
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                //unhook
+                //dispose child objects
+                //set large objects to null
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ AVote-Rewards/Config.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Newtonsoft.Json;
+using TShockAPI;
+
+
+namespace AVote
+{
+	public class Config
+	{
+		public List<string> Commands = new List<string>() { "/give 'meowmere' %PLAYER% 1 legendary" };
+
+		public string apiKey = "xxx";
+
+		public string rewardMessage = "[Vote Rewards] %PLAYER% has voted for us and receieved a reward. Use /vote to get the same reward!";
+		public void Write()
+		{
+			string path = Path.Combine(TShock.SavePath, "AVote.json");
+			File.WriteAllText(path, JsonConvert.SerializeObject(this, Formatting.Indented));
+		}
+		public static Config Read()
+		{
+			string filepath = Path.Combine(TShock.SavePath, "AVote.json");
+
+			try
+			{
+				Config config = new Config();
+
+				if (!File.Exists(filepath))
+				{
+					File.WriteAllText(filepath, JsonConvert.SerializeObject(config, Formatting.Indented));
+				}
+				config = JsonConvert.DeserializeObject<Config>(File.ReadAllText(filepath));
+
+
+				return config;
+			}
+			catch (Exception ex)
+			{
+				TShock.Log.ConsoleError(ex.ToString());
+				return new Config();
+			}
+		}
+	}
+}
